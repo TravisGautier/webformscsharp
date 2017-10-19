26117d1301785a705c1c1007dc322083328dccda
Travis Gautier <tgautier13@gmail.com>
2017-10-19 12:50:12 -0500

    Add files via upload
    Building the Contact Application - Contact.aspx, Contact.aspx.cs

---
+++ Lesson16/Lesson16/Contact.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Lesson16
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                txtMessage.Text = "you clicked on the send button";
+            }
+        }
+    }
+}
